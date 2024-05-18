1cc562e7f503a7afd69c9df11df8869cfe80a241
zakzakst <e1433104@gmail.com>
2024-05-18 23:40:01 +0900

    修正

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,20 @@ export default {
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: function (assetInfo) {
+          const cssRegex = new RegExp(".css$", "i");
+          const imgRegex = new RegExp(".(gif|jpeg|jpg|png|svg|webp)$", "i");
+          if (cssRegex.test(assetInfo.name)) {
+            return "css/[name].min.[ext]";
+          } else if (imgRegex.test(assetInfo.name)) {
+            return "images/[name].[ext]";
+          } else {
+            return "assets/[name].[ext]";
+          }
+        },
+      },
+    },
   },
 };
